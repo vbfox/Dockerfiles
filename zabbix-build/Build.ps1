7a8540123e0b970b213e171736dfc26cffac6a6f
Julien Roncaglia <julien@roncaglia.fr>
2018-06-01 16:51:37 +0200

    Build a recent zabbix agent with PCRE

--- zabbix-build/Build.ps1
+++ zabbix-build/Build.ps1
@@ -1,14 +1,26 @@
 Push-Location
 
-#Cleanup previous runs
+# CLEANUP PREVIOUS RUNS IF ANY
 Remove-Item 'src' -Force  -Recurse -ErrorAction SilentlyContinue
-
-# UNZIP
 mkdir src
-$archive = @(Get-ChildItem -Path 'zabbix' '*.tar.gz')[0].FullName
-& cmd.exe "/C 7za.exe x -so $archive | 7za.exe x -si -ttar -osrc"
+
+# BUILD PCRE
+Push-Location
+$pcreArchive = @(Get-ChildItem -Path 'zabbix' 'pcre-*.zip')[0].FullName
+& .\7za.exe x -osrc $pcreArchive
 cd src
-cd @(Get-ChildItem *)[0]
+cd @(Get-ChildItem 'pcre-*')[0]
+$pcreSrc = (Get-Location).Path
+$env:PATH += ';C:\Program Files\CMake\bin'
+cmake -G "Visual Studio 14 2015 Win64" -DPCRE_SUPPORT_UNICODE_PROPERTIES=ON -DPCRE_SUPPORT_UTF=ON -DCMAKE_C_FLAGS_RELEASE:string="/MT"
+& msbuild PCRE.sln /property:Configuration="Release"
+Pop-Location
+
+# UNZIP ZABBIX
+$zabbixArchive = @(Get-ChildItem -Path 'zabbix' 'zabbix-*.tar.gz')[0].FullName
+& cmd.exe "/C 7za.exe x -so $zabbixArchive | 7za.exe x -si -ttar -osrc"
+cd src
+cd @(Get-ChildItem 'zabbix-*')[0]
 $src = (Get-Location).Path
 Remove-Item 'bin\win64\*' -Force -Recurse -ErrorAction SilentlyContinue
 mkdir 'bin\win64\dev'
@@ -18,10 +30,14 @@ cd build\win32\project
 (Get-Content Makefile_sender_dll).replace('..\..\..\src\zabbix_sender\win32\zabbix_sender.o', '..\..\..\src\zabbix_sender\zabbix_sender.o') | Set-Content Makefile_sender_dll
 
 # BUILD
-& nmake CPU=AMD64 TLS=openssl TLSINCDIR="C:\OpenSSL-Win64\include" TLSLIBDIR="C:\OpenSSL-Win64\lib"
+& nmake CPU=AMD64 TLS=openssl TLSINCDIR="C:\OpenSSL-Win64\include" TLSLIBDIR="C:\OpenSSL-Win64\lib" "PCREINCDIR=$pcreSrc" "PCRELIBDIR=$pcreSrc\Release"
 
-# Extract result
+# MOVE BINARIES OUT OF CONTAINER
 Pop-Location
 mkdir 'zabbix\win64' -ErrorAction SilentlyContinue
 Remove-Item 'zabbix\win64' -Force -Recurse -ErrorAction SilentlyContinue
 Copy-Item "$src\bin\win64\*" 'zabbix\win64' -Recurse -Force
+
+Copy-Item 'C:\OpenSSL-Win64\bin\msvcr*.dll' 'zabbix\win64'
+Copy-Item 'C:\OpenSSL-Win64\bin\libcrypto*.dll' 'zabbix\win64'
+Copy-Item 'C:\OpenSSL-Win64\bin\libssl*.dll' 'zabbix\win64'
--- zabbix-build/Readme.md
+++ zabbix-build/Readme.md
@@ -4,23 +4,26 @@ As Zabbix doesn't provide builds with TLS enabled here is an image that can prod
 
 **NOTE: This image isn't publically pushed as the VS Build Tools license might not allow it, you need to build it yoursef**
 
-## Usage
-
-Create a directory like `C:\Downloads\zabbix` that contains only the current `.tar.gz` source file then run :
-
-```
-docker run -it -v C:\Downloads\zabbix:C:\build\zabbix vbfox/zabbix-build powershell Build.ps1
-```
-
-It will put the result in `C:\Downloads\zabbix\win64`.
-
 ## Building the image
 
 As the VS Build Tools are large the size of the docker builder need to be made larger, see [Microsoft documentation][large-container].
 
 [large-container]: https://docs.microsoft.com/en-us/visualstudio/install/build-tools-container#step-4-expand-maximum-container-disk-size
 
-```
+```cmd
 docker build -t vbfox/zabbix-build:latest -m 4GB .
 ```
 
+## Usage
+
+Create a directory like `C:\Downloads\zabbix` that contains the current `zabbix-*.tar.gz` source file for zabbix and corresponding `pcre-*.zip` source then run :
+
+```cmd
+docker run -it -v C:\Downloads\zabbix:C:\build\zabbix vbfox/zabbix-build powershell Build.ps1
+```
+
+It will put the result in `C:\Downloads\zabbix\win64`.
+
+Tested with
+- Zabbix: https://sourceforge.net/projects/zabbix/files/ZABBIX%20Latest%20Stable/3.4.9/zabbix-3.4.9.tar.gz/download
+- PCRE: ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.41.zip
