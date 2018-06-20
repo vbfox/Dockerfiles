3f9fb931772f631b232daf239c3317d27e12de86
Julien Roncaglia <julien@roncaglia.fr>
2018-06-20 21:44:06 +0200

    Node 10.5.0 in fable-build

--- fable-build/Push.ps1
+++ fable-build/Push.ps1
@@ -1,10 +1,11 @@
 $repo = 'vbfox/fable-build'
 $aspnet_version = '2.1.301'
 $mono_version = '5.12.0.226'
-$node_version = '10.4.1'
+$node_version = '10.5.0'
 $yarn_version = '1.7.0'
 
 $tag = "stretch-aspnet-${aspnet_version}-mono-${mono_version}-node-${node_version}-yarn-${yarn_version}"
+$altTag = "stretch-aspnet-${aspnet_version}-node-${node_version}"
 $shortTag = "stretch"
 
 & docker build . -t "${repo}:latest"
@@ -12,6 +13,9 @@ $shortTag = "stretch"
 
 & docker tag "${repo}:latest" "${repo}:${tag}"
 & docker push "${repo}:${tag}"
+
+& docker tag "${repo}:latest" "${repo}:${altTag}"
+& docker push "${repo}:${altTag}"
 
 & docker tag "${repo}:latest" "${repo}:${shortTag}"
 & docker push "${repo}:${shortTag}"
